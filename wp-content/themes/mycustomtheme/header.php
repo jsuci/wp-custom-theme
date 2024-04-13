362751fac63ffa8aa55cebac8a490a7613b7adab
jammagsuci <jam@inboundfound.com>
2024-04-14 04:02:31 +0800

    use logo at the same time site text

--- wp-content/themes/mycustomtheme/header.php
+++ wp-content/themes/mycustomtheme/header.php
@@ -9,11 +9,9 @@
 
 <body class="flex flex-col gap-y-9 bg-slate-900 text-slate-300 mx-auto px-4 text-lg">
     <header class="flex flex-col pt-8 gap-y-6 md:flex-row md:justify-between md:items-center">
-        <h1 class="logo">
-            <a href="/" aria-label="Home page">
-                <img src="<?php echo get_theme_file_uri('images/bestcheatfiles-logo.png'); ?>" alt="Site Logo">
-            </a>
-        </h1>
+        <a href="/" aria-label="Home page">
+            <div class="site-logo">BestCheatFiles</div>
+        </a>
         <nav aria-label="Main menu" class="p-4">
             <?php wp_nav_menu(array(
                 'theme_location' => 'headerMenuLocation',
