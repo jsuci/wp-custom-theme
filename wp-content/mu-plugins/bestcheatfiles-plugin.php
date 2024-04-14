f0a3703397820be5d695259d2a44e51fd36b1485
jammagsuci <jam@inboundfound.com>
2024-04-15 03:47:57 +0800

    add meta description that uses acf fields

--- wp-content/mu-plugins/bestcheatfiles-plugin.php
+++ wp-content/mu-plugins/bestcheatfiles-plugin.php
@@ -1,51 +1,22 @@
 <?php
 
 
-function adjustDateToPreviousMonthWithRandomTime(string $dateStr): string
+function add_custom_acf_meta_tags()
 {
-    // Parse the given date string
-    $givenDate = DateTime::createFromFormat('Y-m-d', $dateStr);
-    $currentDate = new DateTime();
+    // Check if the function exists and the current post has a valid ID
+    if (function_exists('get_field') && is_singular()) {
+        $post_id = get_the_ID();
 
-    // Adjust the month to one month before the current month, and update the year to the current year
-    $previousMonth = (int)$currentDate->format('m') - 1;
-    $year = $currentDate->format('Y');
+        // Retrieve ACF field value
+        $post_description = get_field('post_description', $post_id);
+        $meta_description = substr($post_description, 0, 160);
 
-    // If the previous month is December (0 after subtraction), adjust the year and month accordingly
-    if ($previousMonth === 0) {
-        $previousMonth = 12;
-        $year--; // Decrease the year if we roll back to December
-    }
-
-    // Apply adjustments
-    $givenDate->setDate($year, $previousMonth, $givenDate->format('d'));
-
-    // Generate random time values
-    $hours = mt_rand(0, 23);
-    $minutes = mt_rand(0, 59);
-    $seconds = mt_rand(0, 59);
-
-    // Set the random time to the given date
-    $givenDate->setTime($hours, $minutes, $seconds);
-
-    // Return the updated date string in "Y-m-d H:i:s" format
-    return $givenDate->format("Y-m-d H:i:s");
-}
-
-
-function update_publish_date($post_id, $new_date)
-{
-    // Check if the new date is in the correct format 'Y-m-d H:i:s'
-    if (DateTime::createFromFormat('Y-m-d H:i:s', $new_date) !== false) {
-        $post_data = array(
-            'ID'            => $post_id,
-            'post_date'     => $new_date, // New publish date
-            'post_date_gmt' => get_gmt_from_date($new_date) // Convert to GMT
-        );
-
-        // Update the post
-        $return_value = wp_update_post($post_data);
-    } else {
-        error_log('Invalid date format. Date must be in the format Y-m-d H:i:s');
+        // Check if the field has a value and output it
+        if ($meta_description) {
+            echo '<meta name="description" content="' . esc_attr($meta_description) . '">' . "\n";
+        }
     }
 }
+
+// Add the function to the wp_head action
+add_action('wp_head', 'add_custom_acf_meta_tags');
